8abba307a649552f8d8d14da9bab63575c6beb63
CJ Romero <cjromero51@yahoo.com>
2018-07-30 08:41:43 -0700

    ES6 updated

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,19 +1,19 @@
 $(function() {
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         //ensures allFeeds is defined and not empty
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
         //loops through each feed to make sure URL is defined and not empty
-        it('URL defined and not empty', function() {
+        it('URL defined and not empty', () => {
             for (feed of allFeeds) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url).not.toBe('');
             };
         });
         //loops through each feed to make sure the name is defined and not empty
-        it('name is defined and not empty', function() {
+        it('name is defined and not empty', () => {
             for (feed of allFeeds) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name).not.toBe('');
@@ -21,14 +21,14 @@ $(function() {
         });
     });
 
-    describe('The menu', function() {
+    describe('The menu', () => {
         var icon = document.querySelector('.icon-list');
         //ensures the menu is hidden by default
-        it('is hidden by default', function() {
+        it('is hidden by default', () => {
             expect($('body').hasClass("menu-hidden")).toBe(true);
         });
         //clicks the icon twice to ensure the menu gets toggled correctly
-        it('changes visibility when clicked', function() {
+        it('changes visibility when clicked', () => {
             icon.click();
             expect($('body').hasClass("menu-hidden")).toBe(false);
             icon.click();
@@ -36,34 +36,34 @@ $(function() {
         });
     });
 
-    describe('Initial Entries', function() {
+    describe('Initial Entries', () => {
         //asynchronous preparation
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach(done => {
+            loadFeed(0, () => {
                 done();
             });
         });
         //makes sure that when loadFeed() completes,
         //the feed container is not empty
-        it('should load a .entry element into .feed', function(done) {
+        it('should load a .entry element into .feed', done => {
             expect($('.feed.entry')).toBeDefined();
             done();
         });
     });
 
-    describe("New Feed Selection", function() {
+    describe("New Feed Selection", () => {
 
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach(done => {
+            loadFeed(0, () => {
                 initialData = document.querySelector('.feed').innerHTML;
-                loadFeed(1, function() {
+                loadFeed(1, () => {
                   newData = document.querySelector('.feed').innerHTML;
                   done();
                 });
             });
         });
         //makes sure the page content changes on different feed inputs
-        it('should check to see if loadFeed() changes the content', function(done) {
+        it('should check to see if loadFeed() changes the content', done => {
             //compares html from each array
             expect(initialData).not.toBe(newData);
             done();
